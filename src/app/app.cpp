f5741914642f55c35b88bee359394010bfa301be
mayumar <maytalbada@gmail.com>
2022-11-20 12:16:40 +0100

    Menu terminado (Mayte)

---
+++ src/app/app.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+int main(){
+
+	int opt;
+
+	do{
+		std::cout<<std::endl;
+		std::cout<<"Introduzca una opcion"<<std::endl;
+		std::cout<<"1. Opcion 1"<<std::endl;
+		std::cout<<"2. Opcion 2"<<std::endl;
+		std::cout<<"3. Opcion 3"<<std::endl;
+		std::cout<<"4. Opcion 4"<<std::endl;
+		std::cout<<"5. Opcion 5"<<std::endl;
+		std::cout<<"6. Salir"<<std::endl;
+		std::cin>>opt;
+		std::cout<<std::endl;
+
+		if(opt<1 || opt>6){
+			std::cout<<"Error: introduzca una de las opciones disponibles"<<std::endl;
+		}
+
+		switch(opt){
+			case 1:
+				std::cout<<"Opcion 1"<<std::endl;
+			break;
+
+			case 2:
+				std::cout<<"Opcion 2"<<std::endl;
+			break;
+
+			case 3:
+				std::cout<<"Opcion 3"<<std::endl;
+			break;
+
+			case 4:
+				std::cout<<"Opcion 4"<<std::endl;
+			break;
+
+			case 5:
+				std::cout<<"Opcion 5"<<std::endl;
+			break;
+		}
+
+	}while(opt!=6);
+
+	return 0;
+}
